1ad6c8a10f5e8ec848f254f79f68f846f101d17b
krzysztof-gasiewski <krzysztof.gasiewski@gmail.com>
2018-01-20 14:06:14 +0100

    first working version

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void funkcja(string tekst){
+    cout << "Linia 1: ";
+    int licznik {2};
+    for(auto &znak : tekst){
+        cout << znak;
+        if(znak == '\n'){
+            cout << "Linia " << licznik++ << ": ";
+        }
+    }
+}
+
+int main()
+{
+    string listDoM{"Narodowa galeria,\nwielka sztuka,\ntego jeszcze nie bylo."};
+    cout << listDoM << endl << endl;
+    funkcja(listDoM);
+
+    return 0;
+}
